0941623d1a2d5e4df7c5ae78ba90269f0c2d075b
ManojKumar-18-24 <manojkumarsamudrala836@gmail.com>
2025-01-19 19:47:27 +0530

    appwrite initialization

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwritePostsId : String(import.meta.env.VITE_APPWRITE_POSTS_ID),
+    appwriteNotificationsId : String(import.meta.env.VITE_APPWRITE_NOTIFICATIONS_ID),
+    appwritePendingRequestsId : String(import.meta.env.VITE_APPWRITE_PENDING_REQUESTS_ID),
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf
