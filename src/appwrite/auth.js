9b049994809db878f9c8961fc689de448b32ccbe
ManojKumar-18-24 <manojkumarsamudrala836@gmail.com>
2025-01-20 12:26:09 +0530

    configure appwrite

---
+++ src/appwrite/auth.js
@@ -0,0 +1,58 @@
+import conf from "../conf/conf";
+import { Client,ID,Account } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = this.account.create(ID.unique(),email,password,name);
+
+            if(userAccount)
+            {
+                return this.login({email,password});
+            }
+        } catch (error) {
+            throw(error)
+        }
+    }
+
+    async login({email,password}) {
+          try {
+             return this.account.createEmailPasswordSession(email.password);
+          } catch (error) {
+            throw(error);
+          }
+    }
+
+    async getCurrentUser() {
+        try{
+            return this.account.get()
+        }
+        catch(error)
+        {
+            throw(error)
+        }
+        return null;
+    }
+     
+    async logout() {
+        try {
+            return this.account.deleteSessions();
+        } catch (error) {
+            throw(error)
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
---
+++ src/appwrite/config.js
@@ -0,0 +1,248 @@
+import conf from "../conf/conf";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({
+    title,
+    model,
+    starting_time,
+    ending_time,
+    image,
+    price,
+    userId,
+  }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        ID.unique(),
+        {
+          title,
+          model,
+          starting_time,
+          ending_time,
+          image,
+          price,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("error in createPost::", error);
+    }
+  }
+
+  async updatePost({
+    id,
+    title,
+    model,
+    starting_time,
+    ending_time,
+    image,
+    price,
+    userId,
+  }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        id,
+        {
+          title,
+          model,
+          starting_time,
+          ending_time,
+          image,
+          price,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log("error in updatePost::", error);
+    }
+  }
+
+  async deletePost(post_id) {
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        post_id
+      );
+      return true;
+    } catch (error) {
+      console.log("error in updatePost::", error);
+      return true;
+    }
+  }
+
+  async getPost(post_id) {
+    try {
+      return await this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        post_id
+      );
+    } catch (error) {
+      console.log("error in getPost::", error);
+      return false;
+    }
+  }
+
+  async getPosts(queries = []) {
+    try {
+      // Get the current time in ISO 8601 format
+      const currentTimeISO = new Date().toISOString();
+
+      // Add a query to filter posts with starting_time > current time
+      queries.push(Query.greater("starting_time", currentTimeISO));
+
+      // Fetch documents with the updated query
+      return await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        queries
+      );
+    } catch (error) {
+      console.log("error in getPosts::", error);
+      return false;
+    }
+  }
+
+  async deletePosts() {
+    try {
+      // Get the current time in ISO 8601 format
+      const currentTimeISO = new Date().toISOString();
+
+      // Fetch all documents where ending_time is less than the current time
+      const postsToDelete = await this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwritePostsId,
+        [Query.less("ending_time", currentTimeISO)]
+      );
+
+      // Loop through each document and delete it
+      for (const post of postsToDelete.documents) {
+        await this.databases.deleteDocument(
+          conf.appwriteDatabaseId,
+          conf.appwritePostsId,
+          post.$id // Document ID to delete
+        );
+      }
+
+      console.log(
+        `${postsToDelete.documents.length} posts deleted successfully.`
+      );
+      return true;
+    } catch (error) {
+      console.log("error in deletePosts::", error);
+      return false;
+    }
+  }
+
+  //notification service
+
+  async createNotification({
+    owner,
+    tenant,
+    post_id,
+  }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteNotificationsId,
+        ID.unique(),
+        {
+          owner,
+          post_id,
+          tenant,
+        }
+      );
+    } catch (error) {
+      console.log("error in createNotification::", error);
+    }
+  }
+
+  async updateNotification({
+    id,
+    owner,
+    tenant,
+    post_id,
+    isRead,
+    isAccepted,
+  }) {
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteNotificationsId,
+        id,
+        {
+          owner,
+          post_id,
+          tenant,
+          isRead,
+          isAccepted,
+        }
+      );
+    } catch (error) {
+      console.log("error in createPost::", error);
+    }
+  }
+
+   async getNotifications(userId){
+       try {
+        return await  this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteNotificationsId,
+            Query.or([Query.equal("owner",userId),Query.equal("tenant",userId)])
+        )
+       } catch (error) {
+        console.log('error in getall Notifications:',error);
+        throw(error)
+       }
+   }
+   
+  //file upload service
+
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("error in uploadFile::", error);
+      return false;
+    }
+  }
+
+  async delteFile(fileId) {
+    try {
+      await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+    } catch (error) {
+      console.log("error in deleteDile::", error);
+      return false;
+    }
+  }
+
+  getFilePreview(fileId) {
+    if (fileId.fileId) fileId = fileId.fileId;
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
+}
+
+const service = new Service();
+
+export default service;
